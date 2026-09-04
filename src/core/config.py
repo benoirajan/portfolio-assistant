30e421c8cfbd01dcbdf47a84327b10b3d4dcad7c
Benoi Abraham <benoi.abraham@ba.com>
2026-09-04 12:19:22 +0530

    chore: add nsepython and yfinance dependencies for NSE market data

--- src/core/config.py
+++ src/core/config.py
@@ -10,12 +10,8 @@ class Settings:
     KITE_API_KEY: str = os.getenv("KITE_API_KEY", "")
     KITE_API_SECRET: str = os.getenv("KITE_API_SECRET", "")
     KITE_REDIRECT_URL: str = os.getenv("KITE_REDIRECT_URL", "http://127.0.0.1:8000/api/v1/auth/callback")
-    
-    # Environment variable support for Zerodha enctoken (Free Live Sync)
     ZERODHA_ENCTOKEN: str = os.getenv("ZERODHA_ENCTOKEN", os.getenv("ENCTOKEN", os.getenv("KITE_ENCTOKEN", "")))
-    
     DEMO_MODE: bool = os.getenv("DEMO_MODE", "true").lower() in ("true", "1", "t")
-    
     APP_HOST: str = os.getenv("APP_HOST", "127.0.0.1")
     APP_PORT: int = int(os.getenv("APP_PORT", "8000"))
 
